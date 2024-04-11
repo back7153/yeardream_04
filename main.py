f8311121af276e0bfeaf0dd57fb65646ea059f14
bongwoon.baek <qhddns529@gmail.com>
2024-04-11 23:48:35 +0900

    make prac6.py

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import prac2
 import prac3
 import prac4_bw
 import prac5_bw
+import prac6
 
 def main():
     parser = argparse.ArgumentParser(description="Run practice Python scripts.")
-    parser.add_argument('script', choices=['prac1', 'prac2', 'prac3', 'prac4_bw', 'prac5_bw'], help='The script to run')
+    parser.add_argument('script', choices=['prac1', 'prac2', 'prac3', 'prac4_bw', 'prac5_bw', 'prac6'], help='The script to run')
     args = parser.parse_args()
 
     if args.script == 'prac1':
@@ -21,6 +22,8 @@ def main():
         prac4_bw.read_file()
     elif args.script == 'prac5_bw':
         prac5_bw.main()
+    elif args.script == 'prac6':
+        prac6.game_loop()
 
 if __name__ == "__main__":
     main()
